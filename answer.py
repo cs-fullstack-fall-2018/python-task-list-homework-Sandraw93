ea69ef2f06f07be695a2037ea1686723c859d3f9
sandra williams <sannwilliams93@yahoo.com>
2018-07-18 15:39:42 -0500

    check in

---
+++ answer.py
@@ -0,0 +1,18 @@
+print()
+
+
+a = ("congratulations! You're running [sandra]'s Task List program. \n")
+p = ("what would you like to do next?")
+b = ("list all task \n")
+c = ("delete a task \n")
+e = ("quit \n")
+print("{}{}{}{}".format(a, p, b, c, e))
+
+user=""
+newtask=""
+removetask=""
+while (user !='q'):
+    user = input( "what would you like to do? + list all task + delete a task, or enter 'q'")
+
+
+
---
+++ p.py
@@ -0,0 +1,17 @@
+#Congratulations! You're running [YOUR NAME]'s Task List program.
+
+#What would you like to do next?
+#1. List all tasks.
+#2. Add a task to the list.
+#3. Delete a task.
+#q. To quit the program
+
+print()
+
+
+a = ("congratulations! You're running [sandra]'s Task List program. \n")
+p = ("what would you like to do next?")
+b = ("list all task \n")
+c = ("delete a task \n")
+e = ("quit \n")
+print("{}{}{}{}".format(a, b, c, e))
